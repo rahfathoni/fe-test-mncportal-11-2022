d336b45e70014a7a6a89098c8c8a14c00c3dc64d
rahfathoni <rahfathoni@gmail.com>
2022-11-23 04:31:30 +0700

    init file

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>this is footer</div>
+</template>
+
+<script>
+export default {
+  /* eslint-disable */
+  name: "FooterLayout",
+};
+</script>
+
+<style></style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderLayout'
+}
+</script>
+
+<style>
+
+</style>
